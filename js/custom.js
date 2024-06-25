32e83fe4bc15ad55edab805dda0fe50da62cbad1
Muhammad Russell <muhammadrussell39@gmail.com>
2024-06-25 22:56:36 +0600

    Add Carousel

--- js/custom.js
+++ js/custom.js
@@ -9,4 +9,25 @@ $(function() {
         $(this).addClass('focused');
     });
 
+    $('.therapy-slider').slick({
+        infinite: true,
+        mobileFirst: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: '.slider-arrow.next',
+        prevArrow: '.slider-arrow.prev',
+        responsive: [{
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        }]
+    });
 });
